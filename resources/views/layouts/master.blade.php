137079e72c4c1c4a3a775b3aa4882a3e9eaf7c7f
baichuanzhen <1109100402@qq.com>
2019-05-02 12:42:33 +0800

    文章循环

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,19 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">   <!-- 因为Bootstrap不支持IE，在IE运行最新的渲染模式 -->
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">  <!-- Ajax提交POST表单防CSRF攻击 -->
     <title>@yield('title')</title>
     <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/normailze.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/bai.css') }}">
     <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/bai.css') }}">
     @yield('my-css')
-
 </head>
 <body>
-<div class="container-fluid" style="width: 1024px;">
+<div class="container" >
     <nav class="navbar navbar-default navbar-static-top">
         <!-- We use the fluid option here to avoid overriding the fixed width of a normal container within the narrow content columns. -->
         <div class="container-fluid">
@@ -33,7 +31,7 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-8">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">主页</a></li>
+                    <li class="active"><a href="{{ url('/') }}">主页</a></li>
                     <li><a href="#">PHP闲谈</a></li>
                     <li><a href="#">Linux</a></li>
                     <li><a href="#">前端</a></li>
